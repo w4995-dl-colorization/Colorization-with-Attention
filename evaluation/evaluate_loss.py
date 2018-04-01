3d6650aab22bb3900375443ee4bfd98d92de795b
Yvonne Zhou <yz2891@columbia.edu>
2018-04-01 16:15:57 -0400

    add helper functions

--- evaluation/evaluate_loss.py
+++ evaluation/evaluate_loss.py
@@ -1,10 +1,12 @@
 # W4995-dl Colorization with attention
 # Evaluate the loss as 1) sum of l2 norms, aka MSE, 2) PSNR peak signal to noise ratio, 
 # 3) deviation in avg color saturation
-# Others in progress: percent deviation in class rebalancing, boundary limitation, boundary smoothness
+# Others in consideration: percent deviation in class rebalancing, boundary limitation, boundary smoothness
 
 from skimage import io, color, measure
 import numpy as np
+import sys
+import glob
 
 
 def L2_lab(pred, gtruth):
@@ -16,7 +18,7 @@ def L2_lab(pred, gtruth):
 		gtruth_lab = color.rgb2lab(gtruth[i])
 		#loss.append((pred[i][1]-gtruth[i][1])**2 + (pred[i][2]-gtruth[i][2])**2)
 		loss.append(measure.compare_mse(gtruth_lab[i],pred_lab[i]))
-	return sum(loss)/len(loss)
+	return "L2 norm (LAB): %f" % (sum(loss)/len(loss))
 
 def L2_AuC(pred,gtruth):
 	for i in range(0, len(pred)):
@@ -24,15 +26,14 @@ def L2_AuC(pred,gtruth):
 		gtruth_lab = color.rgb2lab(gtruth[i]).reshape(len(pred[i])**2,3)
 		loss =  (pred_lab[:,1]-gtruth_lab[:,1])**2 + (pred_lab[:,2]-gtruth_lab[:,2])**2
 		bins = np.arange(151)
-		his, edges = np.histogram(loss,bins)
+		#his, edges = np.histogram(loss,bins)
 		his = his/len(loss)
-
 
 def PSNR_rgb(pred, gtruth):
 	loss = []
 	for i in range(0, len(pred)):
 		loss.append(measure.compare_psnr(gtruth[i],pred[i]))
-	return sum(loss)/len(loss)
+	return "PSNR (RGB): %f" % (sum(loss)/len(loss))
 
 def saturation_hsv(pred, gtruth):
 	loss = []
@@ -42,12 +43,50 @@ def saturation_hsv(pred, gtruth):
 		sat_pred = sum(pred_hsv[i][:,1])
 		sat_truth = sum(gtruth_hsv[i][:,1])
 		loss.append(abs(sat_pred-sat_truth)/sat_truth)
-	return sum(loss)/len(loss)
+	return "Percent Saturation Deviation (HSV): %f" % (sum(loss)/len(loss))
 
 #def color_bleed():
 
+def get_files(folder):
+    """
+    Given path to folder, returns list of files in it
+    """
+    filenames = [file for file in glob.glob(folder+'*/*')]
+    filenames.sort()
+    return filenames
 
-pred_rgb = io.imread(filename_pred)
-gtruth_rgb = io.imread(filename_gtruth)
+def get_img_array(path):
+    """
+    Given path of image, returns it's numpy array
+    """
+    #return scipy.misc.imread(path)
+    return io.imread(path)
+
+def get_images(folder):
+    """
+    returns numpy array of all samples in folder
+    each column is a sample resized to 30x30 and flattened
+    """
+    files = get_files(folder)
+    images = []
+    count = 0
+    
+    for f in files:
+        count += 1
+        if count % 10000 == 0:
+            print("Loaded {}/{}".format(count,len(files)))
+        img_arr = get_img_array(f)
+        #img_arr = img_arr.flatten() / 255.0
+        images.append(img_arr)
+    #X = np.column_stack(images)
+
+    return images
+
+
+pred_rgb = get_images(sys.argv[1])
+gtruth_rgb = get_images(sys.argv[2])
+print(L2_lab(pred_rgb, gtruth_rgb))
+print(PSNR_rgb(pred_rgb,gtruth_rgb))
+print(saturation_hsv(pred_rgb,gtruth_rgb))
 
 
